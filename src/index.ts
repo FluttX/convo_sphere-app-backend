46add302dc260172dbb16ed67028586aee2d527c
Md. Al-Amin <amin15-1951@diu.edu.bd>
2024-12-06 19:07:44 +0600

    feat: socket io implemented to send message

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,55 @@
 import express, {Request, Response} from 'express';
 import {json} from 'body-parser';
+import http from 'http';
+import {Server} from 'socket.io';
 import authRoutes from './routes/authRoutes';
 import conversationRoutes from './routes/conversationsRoutes';
 import messagesRoutes from './routes/messagesRoutes';
+import { saveMessage } from './controllers/messagesController';
 
 const app = express();
+const server = http.createServer(app);
 app.use(json());
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    },
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/conversations', conversationRoutes);
 app.use('/api/messages', messagesRoutes);
 
+
+io.on('connection', (socket) => {
+    console.log('A user connected',socket.id);
+
+    socket.on('joinConversation',(conversationId) => {
+        socket.join(conversationId);
+        console.log(`User ${socket.id} joined conversation ${conversationId}`);
+    });
+
+    socket.on('sendMessage', async (message) => {
+        const {conversationId, senderId, content} = message;
+
+        try {
+            const savedMessage = await saveMessage(conversationId, senderId, content);
+            console.log('sendMessage : ');
+            console.log(savedMessage);
+
+            io.to(conversationId).emit('newMessage', savedMessage);
+            console.log(`Message sent to conversation ${conversationId}`);
+        } catch (error) {
+            console.error('Failed to save message', error);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected', socket.id);
+    });
+});
+
 const PORT = process.env.PORT || 6000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
