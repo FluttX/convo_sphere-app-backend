542295b276808ac79798a90ac86a15abb1c23af0
Md. Al-Amin <amin15-1951@diu.edu.bd>
2024-12-07 00:49:00 +0600

    fix: conversation sql fixed for get all conversation

--- src/controllers/conversationsController.ts
+++ src/controllers/conversationsController.ts
@@ -11,11 +11,15 @@ export const fetchAllConversationsByUserId = async (req: Request, res: Response)
         const query = `
             SELECT 
                 c.id AS conversation_id, 
-                u.username AS participant_name, 
+                CASE
+                    WHEN c.participant_one = $1 THEN u2.username
+                    ELSE u1.username
+                END AS participant_name,
                 m.content AS last_message, 
                 m.created_at AS last_message_time
             FROM conversations c
-            JOIN users u ON (u.id = c.participant_two AND u.id = $1)
+            JOIN users u1 ON u1.id = c.participant_one
+            JOIN users u2 ON u2.id = c.participant_two
             LEFT JOIN LATERAL (
                 SELECT content, created_at
                 FROM messages
